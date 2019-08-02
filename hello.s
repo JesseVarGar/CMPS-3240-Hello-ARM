c423c0733e49bf277257c83d15057fd93d43ed01
Alberto <acruz37@csub.edu>
2019-08-02 00:12:05 +0000

    Assembly example

--- hello.s
+++ hello.s
@@ -1,30 +1,25 @@
+	.arch armv8-a
 	.file	"hello.c"
 	.text
 	.section	.rodata
+	.align	3
 .LC0:
 	.string	"Hello world!"
 	.text
-	.globl	main
-	.type	main, @function
+	.align	2
+	.global	main
+	.type	main, %function
 main:
-.LFB0:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	subq	$16, %rsp
-	movl	$13, -4(%rbp)
-	leaq	.LC0(%rip), %rdi
-	movl	$0, %eax
-	call	printf@PLT
-	movl	$0, %eax
-	leave
-	.cfi_def_cfa 7, 8
+	stp	x29, x30, [sp, -32]!
+	add	x29, sp, 0
+	mov	w0, 13
+	str	w0, [x29, 28]
+	adrp	x0, .LC0
+	add	x0, x0, :lo12:.LC0
+	bl	printf
+	mov	w0, 0
+	ldp	x29, x30, [sp], 32
 	ret
-	.cfi_endproc
-.LFE0:
 	.size	main, .-main
-	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0"
+	.ident	"GCC: (Ubuntu/Linaro 7.4.0-1ubuntu1~18.04.1) 7.4.0"
 	.section	.note.GNU-stack,"",@progbits
