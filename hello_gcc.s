718d71b65d6be42b0898b775deeff60c43e9392d
Alberto <acruz37@csub.edu>
2019-08-02 01:35:28 +0000

    Now using as and ld instead of gcc

---
+++ hello_gcc.s
@@ -0,0 +1,25 @@
+	.arch armv8-a
+	.file	"hello.c"
+	.text
+	.section	.rodata
+	.align	3
+.LC0:
+	.string	"Hello world!"
+	.text
+	.align	2
+	.global	main
+	.type	main, %function
+main:
+	stp	x29, x30, [sp, -32]!
+	add	x29, sp, 0
+	mov	w0, 13
+	str	w0, [x29, 28]
+	adrp	x0, .LC0
+	add	x0, x0, :lo12:.LC0
+	bl	printf
+	mov	w0, 0
+	ldp	x29, x30, [sp], 32
+	ret
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu/Linaro 7.4.0-1ubuntu1~18.04.1) 7.4.0"
+	.section	.note.GNU-stack,"",@progbits
--- makefile
+++ makefile
@@ -1,7 +1,12 @@
-CC=gcc
 
-hello.s: hello.c
-	${CC} -S $< -o $@
+hello.out: hello.o
+	ld hello.o -o hello.out
+
+hello.o: hello.s
+	as hello.s -o hello.o
+
+hello_gcc.s: hello.c
+	gcc -S hello.c -o hello_gcc.s
 
 clean:
 	rm -r -f *.out *.o *.s
